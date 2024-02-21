51fd0fd1fd518322ac59fae6cff30cef23052f78
andersekeberg <anho101@student.kristiania.on>
2024-02-21 06:23:40 +0100

    initial

--- client/components/content.jsx
+++ client/components/content.jsx
@@ -1,6 +1,22 @@
 import { News } from "./news";
+import { useEffect, useState } from "react";
 
 export function Content() {
+  const [article, setArticle] = useState([]);
+  const [newArticle, setNewArticle] = useState("");
+  const [webSocket, setWebSocket] = useState();
+
+  useEffect(() => {
+    new WebSocket("ws://" + window.location.origin);
+  }, []);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    webSocket.send(newMessage);
+    setArticle((current) => [...current, newArticle]);
+    setNewArticle("");
+  }
+
   return (
     <>
       <div id={"container"}>
@@ -8,7 +24,22 @@ export function Content() {
           <News />
         </div>
 
-        <footer>Dette var min avis</footer>
+        <div id={"publish"}>
+          <form onSubmit={handleSubmit}>
+            <input
+              value={newArticle}
+              onChange={(e) => setNewArticle(e.target.value)}
+            />
+            <button>Gikk tom for tid, fungerer ikke</button>
+          </form>
+        </div>
+        <ul id={"newNews"}>
+          {article.map((article) => (
+            <li>{article}</li>
+          ))}
+        </ul>
+
+        <footer></footer>
       </div>
     </>
   );
